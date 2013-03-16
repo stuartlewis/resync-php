2ffe01411d94f3763d8dc45738849e75e32d8937
Stuart Lewis <stuart@stuartlewis.com>
2013-03-16 16:01:49 +0000

    Refactor resourcelist, and allow lazy sync to skip unchanged files even if the date suggests an update

--- ResyncResourcelist.php
+++ ResyncResourcelist.php
@@ -1,27 +1,28 @@
 <?php
 
-include_once('http.php');
-include_once('file.php');
+include_once('util/http.php');
+include_once('util/file.php');
 
-class ResyncResourcelist
-{
+class ResyncResourcelist {
+    // The URL of the resource list
     public $url;
 
+    // The resourcelist raw XML
     private $xml;
 
+    // Whether or not to display debug information
     private $debug = true;
 
+    // Create the new resourcelist
     function __construct($url) {
         $this->url = $url;
-    }
-
-    function load() {
         $xmllist = http_get($this->url);
         $this->xml = simplexml_load_string($xmllist);
         if ($this->debug) print_r($this->xml);
     }
 
-    function baseline($directory, $lastrun, $clear = false, $checksum = true) {
+    // Baseline sync (download everything)
+    function baseline($directory, $lastrun, $clear = false, $checksum = true, $force = false) {
         // First clear the directory?
         if ($clear) {
             rmdir_recursive($directory, false);
@@ -33,7 +34,7 @@ class ResyncResourcelist
 
         // Iterate through each url
         foreach($this->xml->url as $url) {
-            if ($this->debug) echo 'Processing file (' . $count++ . ' of ' . $total . '): ' . $url->loc ."\n";
+            $this->debug('Processing file (' . $count++ . ' of ' . $total . '): ' . $url->loc);
 
             // Create the directory if required
             $parts = explode('/', substr($url->loc, 7));
@@ -42,42 +43,71 @@ class ResyncResourcelist
                 $build = $build . '/' . $parts[$p];
                 if (!file_exists($build)) {
                     mkdir($build);
-                    if ($this->debug) echo 'Adding directory: ' . $build . "\n";
+                    $this->debug(' - Adding directory: ' . $build);
                 }
             }
             $build .= '/' . $parts[count($parts) - 1];
 
+            // Unencode the checksum
+            $namespaces = $url->getNameSpaces(true);
+            $rs = $url->children($namespaces['rs']);
+            $hash = (string)$rs->md[0]->attributes()->hash;
+            $algorithm = substr($hash, 0, strpos($hash, ':'));
+            $cksum = substr($hash, strpos($hash, ':') + 1, (strlen($hash) - strlen($algorithm) - 1));
+
+            // Does the file exist already?
+            $exists = false;
+            if (file_exists($build) && (! $force)) {
+                // Does the checksum match?  If so, we can skip the download later
+                $md5 = base64_encode(md5_file($build, true));
+                $this->debug(' - File already exists, check the checksum:');
+                if ($md5 == $cksum) {
+                    $exists = true;
+                    $this->debug('  - Checksum matches, skipping...');
+                } else {
+                    $this->debug('  - Checksum does not match, not skipping');
+                }
+            }
+
             // Check if file had been updated since last run?
             $lastmod = new DateTime($url->lastmod, new DateTimeZone("UTC"));
-            if ($lastmod > $lastrun) {
-                if ($this->debug) echo 'Downloading file: ' . $build . "\n";
+            if (($lastmod > $lastrun) && (! $exists)) {
+                $this->debug(' - Downloading file: ' . $build);
                 $start = microtime(true);
                 http_get_save($url->loc, $build);
                 $end = microtime(true);
                 $filesize = filesize($build);
                 $timer = $end - $start;
                 $speed = $filesize / $timer;
-                if ($this->debug) echo 'Filesize: ' . $filesize . ' Time: ' . $timer . ' Speed: ' . $speed . "\n";
+                $this->debug('  - Filesize: ' . $filesize . ' Time: ' . $timer . ' Speed: ' . $speed);
+            } else if ($exists) {
+
             } else {
-                if ($this->debug) echo 'Skipping! lastmod: ' . $lastmod->format('Y-m-d H:i:s') .
-                                       ' is before last run: ' . $lastrun->format('Y-m-d H:i:s') . "\n";
+                $this->debug('  - Skipping! lastmod: ' . $lastmod->format('Y-m-d H:i:s') .
+                             ' is before last run: ' . $lastrun->format('Y-m-d H:i:s'));
             }
 
             // Checksum the file?
-            $namespaces = $url->getNameSpaces(true);
-            if ($checksum) {
-                $rs = $url->children($namespaces['rs']);
-                $hash = (string)$rs->md[0]->attributes()->hash;
-                $algorithm = substr($hash, 0, strpos($hash, ':'));
-                $value = substr($hash, strpos($hash, ':') + 1, (strlen($hash) - strlen($algorithm) - 1));
+            if (($checksum) && (!$exists)) {
                 $md5 = base64_encode(md5_file($build, true));
-                if ($this->debug) echo 'Checking checksum: ' . $algorithm . ' with value of ' . $value . "\n";
-                if ($md5 == $value) {
-                    if ($this->debug) echo 'MD5 matched: Expected:' . $value . ' Actual:' . $md5 . "\n";
+                $this->debug(' - Checking checksum: ' . $algorithm . ' with value of ' . $cksum);
+                if ($md5 == $cksum) {
+                    $this->debug('  - MD5 matched: Expected:' . $cksum . ' Actual:' . $md5);
                 } else {
-                    echo 'ERROR MD5 mismatch: Expected:' . $value . ' Actual:' . $md5 . "\n";
+                    echo '  - ERROR MD5 mismatch: Expected:' . $cksum . ' Actual:' . $md5 . "\n";
                 }
             }
         }
     }
+
+    // Whether to display debug messages or not
+    function enableDebug($debug = true) {
+        $this->debug = $debug;
+    }
+
+    // Display a debug mesage
+    private function debug($message) {
+        if ($this->debug) echo $message . "\n";
+    }
 }
+?>
