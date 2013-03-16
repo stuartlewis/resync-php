16fe63decd1963925a7fa8d7855e9b9e8d464e0e
Stuart Lewis <stuart@stuartlewis.com>
2013-03-16 16:01:14 +0000

    Move utility and test classes to new directories

--- test/test.php
+++ test/test.php
@@ -1,13 +1,13 @@
 <?php
     // Test http options
-    include 'http.php';
+    include 'util/http.php';
     echo "Testing http method used:\n";
     show_method();
 
     // Load a test resource list
     include 'ResyncResourcelist.php';
     $resourcelist = new ResyncResourcelist('http://resync.library.cornell.edu/arxiv-q-bio/resourcelist.xml');
-    $resourcelist->load();
+    $resourcelist->enableDebug();
 
     // Baseline download the list (as at 1st Jan 1970)
     $date = new DateTime("1970-01-01T01:00:00Z", new DateTimeZone("UTC"));
