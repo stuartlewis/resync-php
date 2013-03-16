8ecc5984f2f4ad7c862ddc43fd659656ecddee5d
Stuart Lewis <stuart@stuartlewis.com>
2013-03-16 16:25:53 +0000

    Start of test web scripts

---
+++ web/index.php
@@ -0,0 +1,16 @@
+<?php
+
+    include '../config/resync-config.php';
+
+?><html>
+    <head>
+        <title>ResourceSync web test</title>
+    </head>
+    <body>
+        <h1>ResourceSync PHP Client Library</h1>
+        Scripts to test the <a href="http://www.openarchives.org/rs/">ResourceSync</a> <a href="https://github.com/stuartlewis/resync-php">PHP Library</a>.
+        <ul>
+            <li><strong>Test files:</strong> <?=$resync_test_savedir?></li>
+        </ul>
+    </body>
+</html>
