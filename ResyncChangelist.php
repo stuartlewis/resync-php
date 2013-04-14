d0f50cbcd46ed4ec4e011243bf5a4c51040a835d
Stuart Lewis <stuart@stuartlewis.com>
2013-04-14 19:22:18 +0100

    Changelist processing added

--- ResyncChangelist.php
+++ ResyncChangelist.php
@@ -3,7 +3,7 @@
 include_once('util/http.php');
 include_once('util/file.php');
 
-class ResyncCHangelist {
+class ResyncChangelist {
 
     // The URL of the changelist
     public $url;
@@ -44,6 +44,124 @@ class ResyncCHangelist {
         $xmllist = http_get($this->url);
         $xml = simplexml_load_string($xmllist);
         $this->xml = $xml;
+    }
+
+    // Process the change list
+    function process($directory, $lastrun = '', $checksum = true, $force = false, $pretend = false) {
+        // Start the timer
+        $starttime = microtime(true);
+
+        // File counter
+        $total = count($this->xml->url);
+        $count = 1;
+
+        // Was a date given?
+        if ($lastrun == '') {
+            $lastrun = new DateTime("0000-01-01T01:00:00Z", new DateTimeZone("UTC"));
+        }
+
+        // Iterate through each url
+        foreach($this->xml->url as $url) {
+            $namespaces = $url->getNameSpaces(true);
+            $rs = $url->children($namespaces['rs']);
+            $changetype = (string)$rs->md[0]->attributes()->change;
+            $this->debug('Processing change (' . $count++ . ' of ' . $total . '): ' . $url->loc . ' = ' . strtoupper($changetype));
+
+            if (($changetype == 'created') || ($changetype == 'updated')) {
+                // Create the directory if required
+                $parts = explode('/', substr($url->loc, 7));
+                $build = $directory;
+                for ($p = 0; $p < count($parts) - 1; $p++) {
+                    $build = $build . '/' . $parts[$p];
+                    if (!file_exists($build)) {
+                        mkdir($build);
+                        $this->debug(' - Adding directory: ' . $build);
+                    }
+                }
+                $build .= '/' . $parts[count($parts) - 1];
+
+                // Unencode the checksum
+                $hash = (string)$rs->md[0]->attributes()->hash;
+                $algorithm = substr($hash, 0, strpos($hash, ':'));
+                $cksum = substr($hash, strpos($hash, ':') + 1, (strlen($hash) - strlen($algorithm) - 1));
+
+                // Does the file exist already?
+                $exists = false;
+                if (file_exists($build) && (! $force) && (! $pretend)) {
+                    // Does the checksum match?  If so, we can skip the download later
+                    $md5 = base64_encode(md5_file($build, true));
+                    $this->debug(' - File already exists, check the checksum:');
+                    if ($md5 == $cksum) {
+                        $exists = true;
+                        $this->debug('  - Checksum matches, skipping...');
+                        $this->skipcount++;
+                    } else {
+                        $this->debug('  - Checksum does not match, not skipping');
+                    }
+                }
+
+                // Check if file has been updated since last run?
+                $lastmod = new DateTime($url->lastmod, new DateTimeZone("UTC"));
+                if (($lastmod > $lastrun) && (! $exists)) {
+                    $this->debug(' - Downloading file: ' . $build);
+                    if (! $pretend) {
+                        $start = microtime(true);
+                        http_get_save($url->loc, $build);
+                        $filesize = filesize($build);
+                        $end = microtime(true);
+                        $timer = $end - $start;
+                        $speed = $filesize / $timer;
+                        $this->debug('  - Filesize: ' . $filesize . ' Time: ' . $timer . ' Speed: ' . $speed);
+                        $this->downloadsize += $filesize;
+                    } else {
+                        $this->debug('  - PRETEND mode: not really downloading file');
+                    }
+                    $this->downloadcount++;
+                } else if ($exists) {
+
+                } else {
+                    $this->debug('  - Skipping! lastmod: ' . $lastmod->format('Y-m-d H:i:s') .
+                        ' is before last run: ' . $lastrun->format('Y-m-d H:i:s'));
+                    $this->skipcount++;
+                }
+
+                // Checksum the file?
+                if (($checksum) && (!$exists) && (! $pretend)) {
+                    $md5 = base64_encode(md5_file($build, true));
+                    $this->debug(' - Checking checksum: ' . $algorithm . ' with value of ' . $cksum);
+                    if ($md5 == $cksum) {
+                        $this->debug('  - MD5 matched: Expected:' . $cksum . ' Actual:' . $md5);
+                    } else {
+                        echo '  - ERROR MD5 mismatch: Expected:' . $cksum . ' Actual:' . $md5 . "\n";
+                    }
+                }
+
+                if ($changetype == 'created') {
+                    $this->createdcount++;
+                } else {
+                    $this->updatedcount++;
+                }
+            } else if ($changetype == 'deleted') {
+                // Create the directory if required
+                $parts = explode('/', substr($url->loc, 7));
+                $build = $directory;
+                for ($p = 0; $p < count($parts) - 1; $p++) {
+                    $build = $build . '/' . $parts[$p];
+                }
+                $build .= '/' . $parts[count($parts) - 1];
+                echo $build . "\n";
+                if (file_exists($build)) {
+                    echo ' - Deleting file ' . $build . "\n";
+                } else {
+                    echo ' - File does not exist!' . "\n";
+                }
+                $this->deletedcount++;
+            }
+        }
+
+        // End the timer
+        $endtime = microtime(true);
+        $this->downloadtimer = $endtime - $starttime;
     }
 
     // Return the number of created files
--- test/test.php
+++ test/test.php
@@ -44,13 +44,27 @@
         include 'ResyncResourcelist.php';
         $resourcelist = new ResyncResourcelist('http://resync.library.cornell.edu/arxiv/resourcelist.xml');
         $resourcelist->enableDebug();
-
-        // Baseline sync
         $resourcelist->baseline($resync_test_savedir);
         echo $resourcelist->getDownloadedFileCount() . ' files downloaded, and ' .
              $resourcelist->getSkippedFileCount() . ' files skipped' . "\n";
         echo $resourcelist->getDownloadSize() . 'Kb downloaded in ' .
              $resourcelist->getDownloadDuration() . ' seconds (' .
              ($resourcelist->getDownloadSize() / $resourcelist->getDownloadDuration()) . ' Kb/s)' . "\n";
+    }
+
+    if (true) {
+        // Process a changelist
+        include 'ResyncChangelist.php';
+        $changelist = new ResyncChangelist('http://resync.library.cornell.edu/arxiv/changelist.xml');
+        $changelist->enableDebug();
+        $changelist->process($resync_test_savedir);
+        echo ' - ' . $changelist->getCreatedCount() . ' files created' . "\n";
+        echo ' - ' . $changelist->getUpdatedCount() . ' files updated' . "\n";
+        echo ' - ' . $changelist->getDeletedCount() . ' files deleted' . "\n";
+        echo $changelist->getDownloadedFileCount() . ' files downloaded, and ' .
+             $changelist->getSkippedFileCount() . ' files skipped' . "\n";
+        echo $changelist->getDownloadSize() . 'Kb downloaded in ' .
+             $changelist->getDownloadDuration() . ' seconds (' .
+            ($changelist->getDownloadSize() / $changelist->getDownloadDuration()) . ' Kb/s)' . "\n";
     }
 ?>
