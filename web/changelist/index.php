686752e21043023ba63ee39527244d59a5fb614b
Stuart Lewis <stuart@stuartlewis.com>
2013-04-16 18:53:52 +0100

    Web interface to process changelists

--- web/changelist/index.php
+++ web/changelist/index.php
@@ -3,20 +3,33 @@
 include '../../config/resync-config.php';
 
 ?><html>
-<head>
-    <title>ResourceSync web test</title>
-</head>
-<body>
-<h1>ResourceSync PHP Client Library</h1>
+    <head>
+        <title>ResourceSync web test</title>
+    </head>
+    <body>
+    <h1>ResourceSync PHP Client Library</h1>
 
 <?php
-// Test http options
-include '../../util/http.php';
-echo "<strong>Testing http method used:</strong> ";
-show_method();
-echo '<br /><br />';
-echo '<strong>Output:</strong><br />';
+    // Test http options
+    include '../../util/http.php';
+    echo "<strong>Testing http method used:</strong> ";
+    show_method();
+    echo '<br /><br />';
+    echo '<strong>Output:</strong><br />';
 
+    // Load a test changelist
+    include '../../ResyncChangelist.php';
+    $changelist = new ResyncChangelist($_GET['url']);
+    $changelist->enableDebug(true, true);
+    $changelist->process($resync_test_savedir);
+    echo ' - ' . $changelist->getCreatedCount() . ' files created' . "<br />";
+    echo ' - ' . $changelist->getUpdatedCount() . ' files updated' . "<br />";
+    echo ' - ' . $changelist->getDeletedCount() . ' files deleted' . "<br />";
+    echo $changelist->getDownloadedFileCount() . ' files downloaded, and ' .
+        $changelist->getSkippedFileCount() . ' files skipped' . "<br />";
+    echo $changelist->getDownloadSize() . 'Kb downloaded in ' .
+        $changelist->getDownloadDuration() . ' seconds (' .
+        ($changelist->getDownloadSize() / $changelist->getDownloadDuration()) . ' Kb/s)' . "<br />";
 ?>
 </body>
 </html>
