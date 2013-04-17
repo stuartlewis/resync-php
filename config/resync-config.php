1bd4545505399f19bf069d2ba444225fb64a6094
Stuart Lewis <stuart@stuartlewis.com>
2013-04-17 19:22:08 +0100

    Slow down default sync

--- config/resync-config.php
+++ config/resync-config.php
@@ -1,7 +1,7 @@
 <?php
 
     // How many seconds to pause between web requests to a server
-    $resync_delay = 0;
+    $resync_delay = 3;
 
     // Location of saved test files
     $resync_test_savedir = '/resync';
