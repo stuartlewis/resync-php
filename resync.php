83182a4cb0faf61097f8077041465c1b195492c6
Stuart Lewis <stuart@stuartlewis.com>
2013-06-15 07:23:11 +0100

    Including all Resync* function classes

--- resync.php
+++ resync.php
@@ -1,5 +1,9 @@
 <?php
 
-
+// Including this file is an easy way to including all of the ResourceSync function library files
+include_once('ResyncDiscover.php');
+include_once('ResyncCapabilities.php');
+include_once('ResyncResourcelist.php');
+include_once('ResyncChangelist.php');
 
 ?>
